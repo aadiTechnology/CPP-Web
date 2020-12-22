89f9b50444aec0023d9715fad4da897e2a5b0fbc
Sangramskonde <sangramskonde@gmail.com>
2020-12-22 13:31:53 +0530

    Links Remove

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,19 @@ import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     MatSelectModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: [
     FooterComponent,
